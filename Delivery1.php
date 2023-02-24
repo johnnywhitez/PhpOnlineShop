1e8fd11a8677ed9c36e27b426dd0bfb9256cefda
johnnywhitez <78499293+johnnywhitez@users.noreply.github.com>
2023-02-24 00:05:29 +0000

    Php Online Shop
    Simple And Efficient

---
+++ Delivery1.php
@@ -0,0 +1,74 @@
+<?php
+    session_start();
+    require 'connection.php';
+    if(!isset($_SESSION['email'])){
+        header('location: login.php');
+    }
+    $user_id=$_SESSION['id'];
+    $user_products_query="select it.id,it.name,it.price from users_items ut inner join items it on it.id=ut.item_id where ut.user_id='$user_id'";
+    $user_products_result=mysqli_query($con,$user_products_query) or die(mysqli_error($con));
+    $no_of_user_products= mysqli_num_rows($user_products_result);
+    $sum=0;
+    if($no_of_user_products==0){
+        //echo "Add items to cart first.";
+    ?>
+        <script>
+        window.alert("No items in the cart!!");
+        </script>
+    <?php
+    }else{
+        while($row=mysqli_fetch_array($user_products_result)){
+            $sum=$sum+$row['price']; 
+       }
+    }
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="shortcut icon" href="img/lifestyleStore.png" />
+        <title>Tracking</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- latest compiled and minified CSS -->
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css">
+        <!-- jquery library -->
+        <script type="text/javascript" src="bootstrap/js/jquery-3.2.1.min.js"></script>
+        <!-- Latest compiled and minified javascript -->
+        <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+        <!-- External CSS -->
+        <link rel="stylesheet" href="css/style.css" type="text/css">
+    </head>
+    <body>
+        <div>
+            <?php 
+               require 'header.php';
+            ?>
+<br>
+<?php 
+                        $user_products_result=mysqli_query($con,$user_products_query) or die(mysqli_error($con));
+                        $no_of_user_products= mysqli_num_rows($user_products_result);
+                        $counter=1;
+                       while($row=mysqli_fetch_array($user_products_result)){ }
+                           
+                         ?>
+
+<p>Your Package is being Deliveried</p>
+
+<h1>Deliverying Progress</h1>
+<h1>Package has Been Collected At Deport</h1>
+<p>Your Product will be Shipped in: <span id="countdown">5</span> Days... Extimated Time</p>
+
+<!-- Add a JavaScript countdown -->
+<script>
+  // Set the number of seconds to countdown from
+  var seconds = 5;
+
+  // Update the countdown every 1 second
+  setInterval(function() {
+    seconds--;
+    document.getElementById("countdown").innerHTML = seconds;
+    if (seconds == 0) {
+      window.location = "Delivery2.php";
+    }
+  }, 1000);
+</script>
---
+++ sourcecodester_store.sql
@@ -0,0 +1,151 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 24, 2023 at 12:57 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sourcecodester_store`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `items`
+--
+
+CREATE TABLE `items` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `price` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `items`
+--
+
+INSERT INTO `items` (`id`, `name`, `price`) VALUES
+(1, 'Shoes', 300),
+(2, 'Phone Case', 20),
+(3, 'Phone Charger', 50),
+(4, 'Shirt 1', 80),
+(5, 'Titan Model #301', 13000),
+(6, 'Titan Model #201', 3000),
+(7, 'Rolex From Alibaba', 89),
+(8, 'Chinese Shirt', 180),
+(9, 'Hair Dryer', 1500),
+(10, 'Charles', 1000),
+(11, 'HXR', 900),
+(12, 'PINK', 1200);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `contact` varchar(255) NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `contact`, `city`, `address`) VALUES
+(6, 'john eshun', 'sillygoosesad@gmail.com', 'c45917a798f8344815415883c2555fb0', '2111', 'ap', 'a');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users_items`
+--
+
+CREATE TABLE `users_items` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `item_id` int(11) NOT NULL,
+  `status` enum('Added to cart','Confirmed') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `users_items`
+--
+
+INSERT INTO `users_items` (`id`, `user_id`, `item_id`, `status`) VALUES
+(32, 6, 2, 'Confirmed'),
+(35, 6, 4, 'Confirmed'),
+(36, 6, 1, 'Confirmed'),
+(37, 6, 9, 'Confirmed'),
+(39, 6, 2, 'Confirmed'),
+(41, 6, 1, 'Confirmed');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `items`
+--
+ALTER TABLE `items`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users_items`
+--
+ALTER TABLE `users_items`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`,`item_id`),
+  ADD KEY `item_id` (`item_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `items`
+--
+ALTER TABLE `items`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `users_items`
+--
+ALTER TABLE `users_items`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
